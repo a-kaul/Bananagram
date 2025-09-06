3b91dac100bfc9a21e70380d721d9ba2f4db5137
Arjun Kaul <a-kaul@users.noreply.github.com>
2025-09-06 11:09:58 -0700

    Initial Commit

---
+++ Bannanagram/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Bannanagram
+//
+//  Created by Arjun Kaul on 9/6/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
